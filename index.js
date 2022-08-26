2ad7f106b16b71f8ad6409edf9157e362c75fc5c
Gabriel Villaruel <gvill005@gmail.com>
2022-08-25 22:47:47 -0700

    8-25-22 update

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
-function saveLead(){
-    console.log("Button clicked!")
-}
+//grab id
+let inputBtn = document.getElementById("input-btn")
+
+//add event listener to id and log message saying button is clicked from event listener
+inputBtn.addEventListener("click", function(){
+    console.log("Button clicked from addEventListener")
+})
