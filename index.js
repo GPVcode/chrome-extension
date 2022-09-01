a24ed44b0b143c5be2b13696edbb6bbcd6926282
Gabriel Villaruel <gvill005@gmail.com>
2022-08-31 22:34:57 -0700

    first deployment update

--- index.js
+++ index.js
@@ -1,34 +1,26 @@
-//grab id
+//chrome://extensions/
+
 let myLeads = []
-let inputEl = document.getElementById("input-el")
+const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
 
-//add event listener to id and log message saying button is clicked from event listener    
-inputBtn.addEventListener("click", function(){
+inputBtn.addEventListener("click", function() {
     myLeads.push(inputEl.value)
-    console.log(myLeads)
-    //clear input field after submit
-    inputEl.value = "";
-    renderLeads();
-}
+    inputEl.value = ""
+    renderLeads()
+})
 
-)
-
-function renderLeads(){
-let listItems = "";
-for(let i = 0; i < myLeads.length; i++){
-//Simplest way using innerHTML
-    listItems += "<li> <a target='_blank' href='"> + myLeads[i] +"'>" + myLeads[i] + "</a></li>"
-    console.log(listItems)
-//Second Way Using Create Element
-//create element
-    //const newEl = document.createElement("li");
-//set text content
-    //newEl.innerHTML = myLeads[i]
-//append to ul
-    //ulEl.appendChild(newEl)
-}
-//render the listItems inside the unordered list using ulEl.innerHTML
-ulEl.innerHTML = listItems;
+function renderLeads() {
+    let listItems = ""
+    for (let i = 0; i < myLeads.length; i++) {
+        listItems += `
+            <li>
+                <a target='_blank' href='${myLeads[i]}'>
+                    ${myLeads[i]}
+                </a>
+            </li>
+        `
+    }
+    ulEl.innerHTML = listItems  
 }
